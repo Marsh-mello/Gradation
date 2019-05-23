b0cd2ef36ff6e37a497fafbb016c3e0bf209b85b
marsh-mello <1208774030@qq.com>
2019-05-23 14:32:05 +0800

    first commit

---
+++ js/color.js
@@ -0,0 +1,39 @@
+$(function () {
+    $.ajax({
+        url:"color.json",
+        success:function (res) {
+            $.each(res.data,function (i,data) {
+                var Color=data.color;
+                var colorList=$("#gradient");
+                var colorNum1=data.color.indexOf('#');
+                var colorNum2=data.color.lastIndexOf('#')
+                var content1=data.color.substring(colorNum1,colorNum1+7);
+                var content2=data.color.substring(colorNum2,colorNum2+7);
+                colorList.append(
+                    "<li style='--gradient-start:"
+                    +content1
+                    +";--gradient-start-text:\""
+                    +content1
+                    +"\";--gradient-end:"
+                    +content2
+                    +";--gradient-end-text:\""+content2+"\";'><div class='gradient' style='background: linear-gradient("
+                    +Color
+                    +")'></div><div class='start-color' ></div><div class='end-color'></div></li>"
+                )
+            })
+        }
+    });
+    var gradientOn=$('.gradient');
+    var viewLayer=$('#view_layer');
+    gradientOn.click(function () {
+        var viewColor=$(this).css('background');
+        console.log(viewColor)
+        $('body').css('overflow','hidden');
+        viewLayer.css('display','block');
+        viewLayer.css('background',viewColor)
+    })
+    viewLayer.click(function () {
+        $('body').css('overflow','auto');
+        viewLayer.css('display','none');
+    })
+})
